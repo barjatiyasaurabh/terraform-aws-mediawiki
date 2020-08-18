56f79337d890934dc17c01230a1ce2645d186b80
Saurabh Barjatiya <saurabh@sbarjatiya.com>
2020-08-18 14:19:39 +0530

    Created terraform key.  Updated gitignore to ignore terraform key.  Created mysql-root-password file to be ignored by git and copied by terraform to both instances

--- mediawiki.tf
+++ mediawiki.tf
@@ -16,6 +16,11 @@ resource "aws_key_pair" "saurabh-labpc" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxvplzAxWC3DfOnbb7EMZCEOKiI5ALDLVFT56czeyKAO6MY5D1E79f74t1HmIHwpoAOekXdnu580zC7Ni5Wynk6M4YnPfHF2cs4hhMBFttxUWsrI3Cqfyb8zsjyddXKg0XbPj/BvlqA8SBHQX7DGd4gXTXJXHeySvu5y8Rd0f9mpzHYk9+lW06Q3uyON9jzL/PeRD8h8uHb1vqi6+V5R0ShHc1ptlDH90DKd3pgm/cer7WJHNlqsQybAf0ypcfqzDPtywgccTFgnfwnltb3WYCbXcMmrjLCXXrQ8qZMTkZzpnPVT3/FfmaaN9OCvG266kPU/O2s3zxeaaHRWq2aESIQ== saurabh@labpc"
 }
 
+resource "aws_key_pair" "terraform" {
+  key_name   = "terraform"
+  public_key = file("terraform.pub")
+}
+
 resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
@@ -71,7 +76,7 @@ resource "aws_security_group" "webserver" {
 resource "aws_instance" "mediawiki" {
   ami           = "ami-01ca03df4a6012157"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.saurabh-labpc.key_name
+  key_name = aws_key_pair.terraform.key_name
   vpc_security_group_ids = [aws_security_group.webserver.id]
   tags = {
     Name = "Mediawiki"
@@ -79,8 +84,12 @@ resource "aws_instance" "mediawiki" {
   connection {
     type        = "ssh"
     user        = "centos"
-    private_key = file("~/.ssh/id_rsa")
+    private_key = file("terraform")
     host        = self.public_ip
+  }
+  provisioner "file" {
+    source      = "mysql-root-password"
+    destination = "/root/mysql-root-password"
   }
   provisioner "file" {
     source      = "setup_httpd.sh"
@@ -106,10 +115,30 @@ resource "aws_eip" "mediawiki-ip" {
 resource "aws_instance" "mariadb" {
   ami           = "ami-01ca03df4a6012157"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.saurabh-labpc.key_name
+  key_name = aws_key_pair.terraform.key_name
   vpc_security_group_ids = [aws_security_group.webserver.id]
   tags = {
     Name = "Mariadb"
+  }
+  connection {
+    type        = "ssh"
+    user        = "centos"
+    private_key = file("terraform")
+    host        = self.public_ip
+  }
+  provisioner "file" {
+    source      = "mysql-root-password"
+    destination = "/root/mysql-root-password"
+  }
+  provisioner "file" {
+    source      = "setup_mariadb.sh"
+    destination = "/root/setup_mariadb.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /root/setup_mariadb.sh",
+      "/root/setup_mariadb.sh"
+    ]
   }
 }
 
