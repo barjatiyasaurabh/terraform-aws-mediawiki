0faf368403343e0920d52839c6309ed34055d371
Saurabh Barjatiya <saurabh@sbarjatiya.com>
2020-08-18 13:06:59 +0530

    Created security group via terraform so that the code is more portable

--- mediawiki.tf
+++ mediawiki.tf
@@ -16,11 +16,56 @@ resource "aws_key_pair" "saurabh-labpc" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxvplzAxWC3DfOnbb7EMZCEOKiI5ALDLVFT56czeyKAO6MY5D1E79f74t1HmIHwpoAOekXdnu580zC7Ni5Wynk6M4YnPfHF2cs4hhMBFttxUWsrI3Cqfyb8zsjyddXKg0XbPj/BvlqA8SBHQX7DGd4gXTXJXHeySvu5y8Rd0f9mpzHYk9+lW06Q3uyON9jzL/PeRD8h8uHb1vqi6+V5R0ShHc1ptlDH90DKd3pgm/cer7WJHNlqsQybAf0ypcfqzDPtywgccTFgnfwnltb3WYCbXcMmrjLCXXrQ8qZMTkZzpnPVT3/FfmaaN9OCvG266kPU/O2s3zxeaaHRWq2aESIQ== saurabh@labpc"
 }
 
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "webserver" {
+  name        = "webserver"
+  description = "Allow web traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 0
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
 resource "aws_instance" "mediawiki" {
   ami           = "ami-01ca03df4a6012157"
   instance_type = "t2.micro"
   key_name = aws_key_pair.saurabh-labpc.key_name
-  vpc_security_group_ids = ["sg-099110fd1d8ddf519"]
+  vpc_security_group_ids = [aws_security_group.webserver.id]
   tags = {
     Name = "Mediawiki"
   }
@@ -39,7 +84,7 @@ resource "aws_instance" "mariadb" {
   ami           = "ami-01ca03df4a6012157"
   instance_type = "t2.micro"
   key_name = aws_key_pair.saurabh-labpc.key_name
-  vpc_security_group_ids = ["sg-099110fd1d8ddf519"]
+  vpc_security_group_ids = [aws_security_group.webserver.id]
   tags = {
     Name = "Mariadb"
   }
